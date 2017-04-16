bd409db08f2ea8ba5e6590ceeac34eacb68022f9
Hiroshi Nagata <hirosn@live.com>
2017-04-16 16:26:47 -0700

    remove vars.

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,19 +4,20 @@ $(function() {
     url: 'https://www.codeschool.com/users/hirosn.json',
     dataType: 'jsonp',
     success: function(response) {
-      if (response && response.courses &&
-        Array.isArray(response.courses.completed)) {
-          var $badges = $('#badges');
-          response.courses.completed.forEach(function(item) {
-            var $div = $('<DIV></DIV>').addClass('course');
-            $div.append($('<H3></H3>').text(item.title));
-            $div.append($('<IMG>').attr('src', item.badge));
-            $div.append(
-              $('<A target="_blank" class="btn btn-primary">See Course</A>')
-              .attr('href', item.url));
-            $badges.append($div);
-          });
-      }
+      if (!response || !response.courses) { return; }
+      if (!Array.isArray(response.courses.completed)) { return; }
+      response.courses.completed.forEach(function(item) {
+        $('#badges').append(
+          $('<DIV></DIV>')
+          .addClass('course')
+          .append($('<H3></H3>').text(item.title))
+          .append($('<IMG>').attr('src', item.badge))
+          .append(
+            $('<A target="_blank" class="btn btn-primary">See Course</A>')
+            .attr('href', item.url)
+          )
+        );
+      });
     }
   });
 
