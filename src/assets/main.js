0b432d3dade2127e962d3b509245c131697ebfb5
Hiroshi Nagata <hirosn@live.com>
2017-04-16 16:18:42 -0700

    fix vars and remove log.

--- src/assets/main.js
+++ src/assets/main.js
@@ -6,16 +6,15 @@ $(function() {
     success: function(response) {
       if (response && response.courses &&
         Array.isArray(response.courses.completed)) {
-          var badges$ = $('#badges');
+          var $badges = $('#badges');
           response.courses.completed.forEach(function(item) {
-            var div$ = $('<DIV></DIV>').addClass('course');
-            div$.append($('<H3></H3>').text(item.title));
-            div$.append($('<IMG>').attr('src', item.badge));
-            div$.append(
+            var $div = $('<DIV></DIV>').addClass('course');
+            $div.append($('<H3></H3>').text(item.title));
+            $div.append($('<IMG>').attr('src', item.badge));
+            $div.append(
               $('<A target="_blank" class="btn btn-primary">See Course</A>')
               .attr('href', item.url));
-            badges$.append(div$);
-            console.log(item);
+            $badges.append($div);
           });
       }
     }
