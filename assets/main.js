d73d936e526b6877e7e3c5c0b7fd1b8b60733f54
Hiroshi Nagata <hirosn@live.com>
2017-04-16 16:09:38 -0700

    Satisfy the list of requirements.

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,24 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/hirosn.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      if (response && response.courses &&
+        Array.isArray(response.courses.completed)) {
+          var badges$ = $('#badges');
+          response.courses.completed.forEach(function(item) {
+            var div$ = $('<DIV></DIV>').addClass('course');
+            div$.append($('<H3></H3>').text(item.title));
+            div$.append($('<IMG>').attr('src', item.badge));
+            div$.append(
+              $('<A target="_blank" class="btn btn-primary">See Course</A>')
+              .attr('href', item.url));
+            badges$.append(div$);
+            console.log(item);
+          });
+      }
+    }
+  });
 
 });
